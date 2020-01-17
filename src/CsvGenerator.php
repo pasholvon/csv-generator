67ebcac25d1cb998ec0dc57933093c9c6fb74d61
maciej <maciej.jakubowski@grupa.it>
2020-01-17 14:17:36 +0100

    Added Delimiter setter and added exceptions for setters.

--- src/CsvGenerator.php
+++ src/CsvGenerator.php
@@ -16,6 +16,17 @@ class CsvGenerator {
   }
 
   /**
+   * @param mixed $delimiter
+   */
+  public function setDelimiter(string $delimiter) {
+    if (strlen($delimiter) <= 1) {
+      $this->delimiter = $delimiter;
+    } else {
+      trigger_error('Delimiter can be maximum one character.', E_USER_ERROR);
+    }
+  }
+
+  /**
    * @return mixed
    */
   public function getDelimiter() {
@@ -25,8 +36,12 @@ class CsvGenerator {
   /**
    * @param mixed $enclosure
    */
-  public function setEnclosure($enclosure) {
-    $this->enclosure = $enclosure;
+  public function setEnclosure(string $enclosure) {
+    if (strlen($enclosure) <= 1) {
+      $this->enclosure = $enclosure;
+    } else {
+      throw new \Exception('Enclosure can be maximum one character.');
+    }
   }
 
   /**
